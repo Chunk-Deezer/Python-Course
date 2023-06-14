b05199b0fc25e60b4849add4ffa4304d8a2776ee
Chunk-Deezer <135858153+Chunk-Deezer@users.noreply.github.com>
2023-06-14 19:25:27 +0100

    June work

--- 14_June.py
+++ 14_June.py
@@ -6,7 +6,6 @@ print(x) #This line will output variable x, but not y
 #This is called unpacking:
 ingredients=["cake" , "butter" , "sausage"]
 a,b,c=ingredients #This means the variables a,b and c take the values of the words in the list
-
 #Here i will output the ingredients, all in one line of code
 print(a,b,c)
 
