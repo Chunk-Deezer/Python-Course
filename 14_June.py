98a141c23fe812aa963f0b0488ae6c05a2bf7f3b
Chunk-Deezer <135858153+Chunk-Deezer@users.noreply.github.com>
2023-06-14 19:04:28 +0100

    variables, unpacking

--- 14_June.py
+++ 14_June.py
@@ -1,6 +1,7 @@
-print("hello world")
 #This is an example of some variables:
 x=8
-y="clank"
-z=13.0
+y="hello"
 print(x)
+#This is called unpacking:
+ingredients=["cake" , "butter" , "sausage"]
+a,b,c=ingredients #This means the variables a,b and c take the values of the words in the list
