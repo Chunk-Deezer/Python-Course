7a3ffe2eabb4369c50643c1df45e971a76e2d90d
Chunk-Deezer <135858153+Chunk-Deezer@users.noreply.github.com>
2023-06-23 19:18:05 +0100

    Number types

--- 23_June.py
+++ 23_June.py
@@ -2,9 +2,9 @@
 x=8908 #This is data type int
 y=8.908 #this is data type float
 z=8908j #This is data type complex
-#you can change data types from one to another
+#you can change data types from one to another, this is called casting
 a=float(x) #Changes to float
-b=int(z) #Changes to int
+b=int(y) #Changes to int
 #c=complex(y) #This will not work, you cannot change a number to complex type
 print(type(a))
 print(type(b))
@@ -12,3 +12,6 @@ print(type(b))
 import random #This is a random function that is built in
 
 print(random.randrange(1, 10))
+
+f="joules"
+print(f[3]) #this will print the 4th character (character 1 is counted as line 0)
