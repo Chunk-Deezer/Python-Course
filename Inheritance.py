6e8c26f7a576941163da9b3543bb463ba0236e57
Chunk-Deezer <135858153+Chunk-Deezer@users.noreply.github.com>
2024-05-14 18:05:14 +0100

    Inheritance

---
+++ Inheritance.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+x = Person("John", "Doe") #using the person class to create an object
+x.printname() #executing printname method
+
+class Student(Person): #this class named student inherits the functionality from another class, because the parent class is the 
+    pass                                                                                                              #parameter
+
+y = Student("Glenn", "Turner") #the student class has the exact same properties and methods as the Person class
+y.printname()
+
+class student(Person):
+    def __init__(self, fname, lname): #when the __init__ function is added the child will no longer inherent the parent's __init__ func
+        Person.__init__(fname, lname) #to keep the inheritance of the parent's __init__ function, a call to the parent's __init__
+                                                                                                           #function must be made
+#now the child class has the __init__ function and has kept the inheritance of the Person class it can be used to add functionality
+#there is also a super() function which will make the child class inherit all methods and properties from its parent without having
+#to name the parent specifically. it is used in the same place as Person is
+
+class student2(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.year = year
